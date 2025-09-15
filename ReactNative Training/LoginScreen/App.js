5dd7df018e88c048ef87df46e385c012d53d35e3
simran1283 <simran38511@gmail.com>
2025-09-15 11:08:15 +0530

    Re-added LoginScreen as a normal folder

---
+++ ReactNative Training/LoginScreen/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './src/components/LoginScreen';
+export default function App() {
+  return (
+    <LoginScreen/>
+  );
+}
+
---
+++ ReactNative Training/LoginScreen/src/components/LoginScreen.tsx
@@ -0,0 +1,101 @@
+import { View, StyleSheet, Image, Text, TextInput, TouchableOpacity, Pressable, Alert } from "react-native"
+import { vs, s } from "react-native-size-matters"
+
+const LoginScreen = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.innerContainer}>
+                <Image
+                    source={require("../../assets/user.png")}
+                    style={styles.userImage}></Image>
+                <Text style={styles.label}>Username</Text>
+                <TextInput style={styles.input}></TextInput>
+                <Text style={styles.label}>Password</Text>
+                <TextInput style={styles.input} secureTextEntry></TextInput>
+                <Text style={styles.label2} onPress={()=> Alert.alert("Forgot Password Pressed")}>Forgot Password?</Text>
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.8}
+                    onPress={() => Alert.alert("Login Pressed")}
+                >
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+                <Text style={styles.label4}>
+                    New User?{' '}
+                    <Text style={styles.label3} onPress={() => Alert.alert('Sign Up pressed')}>
+                        Sign Up
+                    </Text>
+                </Text>
+
+            </View>
+        </View>
+    )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        height: "100%",
+        width: "100%",
+        backgroundColor: "#263238",
+    },
+    innerContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "flex-start",
+        marginTop : "40%"
+    },
+    userImage: {
+        height: vs(100),
+        width: s(100),
+        resizeMode: "center",
+        marginBottom: 30
+    },
+    label: {
+        alignSelf: "flex-start",
+        fontSize: 15,
+        color: "#90A4AE",
+        marginLeft: "12%",
+        marginTop: '4%'
+    },
+    input: {
+        width: "75%",
+        borderBottomWidth: 2,
+        borderBottomColor: "#90A4AE",
+        marginVertical: 10,
+        color: "white"
+    },
+    label2: {
+        alignSelf : "flex-end",
+        fontSize: 15,
+        color: "#90A4AE",
+        marginRight: "12%",
+        marginVertical: '2%'
+    },
+    button: {
+        paddingVertical: 14,
+        paddingHorizontal: "30%",
+        borderRadius: 112,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+        marginBottom : 6,
+        backgroundColor: "#81C784"
+    },
+    buttonText: {
+        fontSize: 20,
+        color: "#263238"
+    },
+    label3: {
+        fontSize: 15,
+        color: "#A5D6A7",
+        fontWeight:"bold"
+    },
+    label4: {
+        alignSelf: "center",
+        fontSize: 15,
+        color: "#90A4AE"
+    }
+})
